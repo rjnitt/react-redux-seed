034fb3d7048bf06feab90aabe64b259b4fed7fde
mypc <mypc@mypcs-MacBook-Pro.local>
2018-03-16 01:52:13 +0530

    initial commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { connect } from "react-redux"
+
+import Button from 'material-ui/Button';
+
+import { fetchUser } from "../actions/userActions"
+import { fetchTweets } from "../actions/tweetsActions"
+
+@connect((store) => {
+  return {
+    user: store.user.user,
+    userFetched: store.user.fetched,
+    tweets: store.tweets.tweets,
+  };
+})
+export default class Layout extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchUser())
+  }
+
+  fetchTweets() {
+    this.props.dispatch(fetchTweets())
+  }
+
+  render() {
+    const { user, tweets } = this.props;
+
+    if (!tweets.length) {
+      return(
+        <Button variant="raised" color="primary" onClick={this.fetchTweets.bind(this)}>
+          load tweets
+        </Button>
+      )
+    }
+
+    const mappedTweets = tweets.map(tweet => <li key={tweet.id}>{tweet.text}</li>)
+
+    return <div>
+      <h1>{user.name}</h1>
+      <ul>{mappedTweets}</ul>
+    </div>
+  }
+}
